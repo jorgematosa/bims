3b499bc3a26c76499083ebd88223a7a987934742
Jorge Matosa <jorge.m.matosa@gmail.com>
2018-01-10 16:53:30 +0000

    Firebase and bootstrap installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyABrMjW8wNCzNAyHz8JOh2zVjtcOorhEgI',
+      authDomain: 'https://bims-3bf9d.firebaseio.com/'
+    });
+  }
 }
