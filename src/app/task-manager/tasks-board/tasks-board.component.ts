94b952f787f6bcaa895b002ef79ee08bb3cc9767
Jorge Matosa <jorge.m.matosa@gmail.com>
2018-01-16 14:06:13 +0000

    adjusted kanban board to respond to project selected

--- src/app/task-manager/tasks-board/tasks-board.component.ts
+++ src/app/task-manager/tasks-board/tasks-board.component.ts
@@ -1,3 +1,5 @@
+import { Subscription } from 'rxjs/Subscription';
+import { Project } from './../project.model';
 import { Task } from './../task.model';
 import { TasksService } from './../tasks.service';
 import { Component, OnInit } from '@angular/core';
@@ -10,11 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksBoardComponent implements OnInit {
   tasks: Task[];
+  projects: Project[];
+  projectSelected: number;
+  subscription: Subscription;
 
   constructor(private tasksService: TasksService) { }
 
   ngOnInit() {
+    this.projectSelected = this.tasksService.projectSelected;
+    this.subscription = this.tasksService.projectSelectedEvent.subscribe(
+      (index: number) => {
+        this.projectSelected = index;
+      }
+    );
     this.tasks = this.tasksService.getTasks();
+    this.projects = this.tasksService.getProjects();
   }
 
 
