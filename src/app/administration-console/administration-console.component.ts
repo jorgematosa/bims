ff60828507109b4a0689f2cb8a70110dd9cf7608
Jorge Matosa <jorge.m.matosa@gmail.com>
2018-02-20 19:07:07 +0000

    added sidebar to administration console

--- src/app/administration-console/administration-console.component.ts
+++ src/app/administration-console/administration-console.component.ts
@@ -1,17 +1,44 @@
+import { User } from './../auth/user.model';
+import { Subscription } from 'rxjs/Subscription';
 import { AuthService } from './../auth/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-administration-console',
   templateUrl: './administration-console.component.html',
   styleUrls: ['./administration-console.component.css']
 })
-export class AdministrationConsoleComponent implements OnInit {
+export class AdministrationConsoleComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  loggedUser: User = null;
 
-  constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService) { }
 
   ngOnInit() {
+    this.subscription = this.authService.usersLoaded.subscribe(
+      (flag: boolean) => {
+        if (flag === true) {
+          this.loggedUser = this.authService.getLoggedUser();
+          // this.router.navigate(['ticketing-options'], {relativeTo: this.route});
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  isLoaded() {
+    if (this.loggedUser !== null) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   onRegister() {
@@ -24,4 +51,5 @@ export class AdministrationConsoleComponent implements OnInit {
     this.authService.updatingUserSub.next(true);
     this.router.navigate(['register'], {relativeTo: this.route});
   }
+
 }
